1f22f6e8f80f88d0924dc5e3fb349cf92ac8e445
invisiblek <dan.pasanen@gmail.com>
2014-05-12 15:22:57 -0500

    m8: enable qcom time services
    Change-Id: I4f21a4ba29b719111617e4a6f729a75a04160765

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,6 +117,9 @@ BOARD_PROVIDES_LIBRIL := true
 # RPC
 TARGET_NO_RPC := true
 
+# Time services
+BOARD_USES_QC_TIME_SERVICES := true
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn
