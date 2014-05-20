47ec96bc21bd2f6284b47850b676c9c46973df75
Sean Hoyt <seanhoyt963@gmail.com>
2014-05-20 15:45:35 -0500

    Switch back to liquid kernel and remove cpu stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,9 +60,9 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/m8/mkbootimg.mk
-TARGET_KERNEL_CONFIG := cm_m8_defconfig
+TARGET_KERNEL_CONFIG := liquid_m8_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm8974
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := sm-arm-eabi-4.7
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := sm-arm-eabi-4.10
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
